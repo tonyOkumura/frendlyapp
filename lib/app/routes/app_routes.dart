b244d6a4b4bbba6d5eca1985118d1d4c7bc4d3e9
Tokumura <anton.tereshchenko.0003@bk.ru>
2024-05-17 12:35:45 +0300

    redisign

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,11 +4,9 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
-  static const SPLASH = _Paths.SPLASH;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
-  static const SPLASH = '/splash';
 }
